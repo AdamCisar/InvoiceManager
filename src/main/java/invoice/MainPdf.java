1bcae4b4e220a8bf0f255f8af53e0a898f4d195f
AdamCisar <adamcisar7@gmail.com>
2023-03-28 20:45:05 +0200

    added condition if oldtxt file exists in MainPdf

--- src/main/java/invoice/MainPdf.java
+++ src/main/java/invoice/MainPdf.java
@@ -19,7 +19,12 @@ public class MainPdf {
 		readerPDF = new PdfReader(oldPdf);
 		String parsedText = readerPDF.parseText();
 		
-		oldTxt = new File(getClass().getResource("/textFiles/").getPath()+File.separator+"FileThatWorksForInvoiceApp.txt");
+		oldTxt = new File(System.getProperty("user.home")+File.separator+"FileThatWorksForInvoiceApp.txt");
+		
+		if(oldTxt.exists()) {
+			oldTxt.delete();
+		}
+		
     	txt = new TxtWriter(parsedText, oldTxt.getAbsolutePath());
     	
     	LineReader lr = new LineReader(oldTxt.getAbsolutePath(), oldPdf);
